92737f25e1c92dfd1b9825012eea8f8aff6aeae7
MavisGuo <740430795@qq.com>
2018-08-28 20:16:51 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springdatajpa/Person.java
@@ -0,0 +1,84 @@
+package com.atguigu.springdatajpa;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "jpa_persons")
+@Entity
+public class Person {
+
+    private Integer id;
+    private String lastName;
+    private String email;
+    private Date birth;
+
+    private Address address;
+    private Integer addressId;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    @Column(name = "add_id")
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    @JoinColumn(name = "address_id")
+    @ManyToOne
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", lastName=" + lastName + ", email=" + email + ", birth=" + birth + "]";
+    }
+
+}
---
+++ src/main/java/com/atguigu/springdatajpa/PersonDao.java
@@ -0,0 +1,7 @@
+package com.atguigu.springdatajpa;
+
+public interface PersonDao {
+
+    void test();
+
+}
